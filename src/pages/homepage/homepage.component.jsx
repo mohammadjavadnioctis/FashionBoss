fe7029a927323b20cd444dff793a9c33bd600831
= <mohammadjavadnoctis3@gmail.com>
2021-08-10 11:57:34 -0700

    costumers slider created

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Directory from '../../components/directory/directory.component';
 import Banners from '../../components/banners/banners.component'
+import SwiperMContainer from '../../components/costumer-slider/costumerSlider.component';
 
 import './homepage.styles.scss';
 
 const HomePage = () => (
   <div className='homepage'>
+    <SwiperMContainer />
     <Directory />
     <Banners/>
     <Directory />
